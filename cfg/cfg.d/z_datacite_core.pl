af8e62b70bca21b45b74ef773da8bcb448c6f098
Timothy Miles-Board <timothy.miles-board@london.ac.uk>
2014-12-19 16:07:16 +0000

    Fix })}) mashup in z_datacite_core.pl, new epmi

--- cfg/cfg.d/z_datacite_core.pl
+++ cfg/cfg.d/z_datacite_core.pl
@@ -1,22 +1,21 @@
-# Addes the miniting plugin to the EP_TRIGGER_STATUS_CHANGE
+# Adds the minting plugin to the EP_TRIGGER_STATUS_CHANGE
 $c->add_dataset_trigger( "eprint", EP_TRIGGER_STATUS_CHANGE , sub {
-       my ( %params ) = @_;
- 
-       my $repository = %params->{repository};
- 
-       return undef if (!defined $repository);
-	
-       if (defined %params->{dataobj}) {
-               my $dataobj = %params->{dataobj};
-               my $eprint_id = $dataobj->id;
+	my ( %params ) = @_;
 
-			$repository->dataset( "event_queue" )->create_dataobj({
-						                       pluginid => "Event::DataCiteEvent",
-						                       action => "datacite_doi",
-						                       params => [$dataobj->internal_uri],
-						               });
-		               });
-     }
- 
+	my $repository = %params->{repository};
+
+	return undef if (!defined $repository);
+
+	if (defined %params->{dataobj})
+	{
+		my $dataobj = %params->{dataobj};
+		my $eprint_id = $dataobj->id;
+
+		$repository->dataset( "event_queue" )->create_dataobj({
+			pluginid => "Event::DataCiteEvent",
+			action => "datacite_doi",
+			params => [$dataobj->internal_uri],
+		});
+	}
 });
- 
+
