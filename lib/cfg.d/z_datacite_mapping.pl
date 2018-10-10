de45ae9ffc6c539db1bad7b6d3402d5738229d5a
Karl Goetz <karl.goetz@utas.edu.au>
2018-10-10 15:23:47 +1100

    Add validation for 'type' before minting
    I've just had a situation where the type of my eprint wasn't known in typemap
    so things fell over at the api submission stage. I've added some validation in
    this commit but haven't included the item type - I haven't checked if its site
    specific yet.

--- lib/cfg.d/z_datacite_mapping.pl
+++ lib/cfg.d/z_datacite_mapping.pl
@@ -532,6 +532,16 @@ $c->{validate_datacite} = sub
 				dates=>$dates );
 	}
 
+	# If we don't have a type or its not in our mapping, thats bad
+	if ( !$dataobj->exists_and_set("type") || !$repository->get_conf("datacitedoi", "typemap", $dataobj->value("type")))
+	{
+		my $types = $xml->create_element( "span", class=>"ep_problem_field:type" );
+
+		push @problems, $repository->html_phrase(
+				"datacite_validate:need_mapped_type",
+				types=>$types );
+	}
+
 	return( @problems );
 };
 
